fb9e9b6b152c71b9a4c6229a4178b01db1ed6fb5
ninavaldez <78568283+ninavaldez@users.noreply.github.com>
2021-02-11 14:43:49 -0800

    Initial commit

---
+++ Assets/EventsManagerScript.cs
@@ -0,0 +1,11 @@
+﻿using System;
+// using System.Collections.Generic;
+using UnityEngine;
+
+public class EventsManagerScript : MonoBehaviour
+{
+    public void MessageConsole()
+    {
+        Debug.Log("Button Clicked TimeStamp: " + DateTime.Now);
+    }
+}
---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+using System.IO;
+
+
+public class GameManagerScript : MonoBehaviour
+{
+    public string filepath = "textfile.txt";
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateFile();
+        WriteFile();
+    }
+   
+    // Question 1: Text File Generation and Writing
+
+    private void CreateFile()
+    {
+        //using a relative path "textfile.txt"
+
+        if (!File.Exists(filepath))
+        {
+            File.Create(filepath).Close();
+        }
+    }
+
+    private void WriteFile()
+    {
+        using (StreamWriter write = new StreamWriter(filepath))
+        {
+            write.WriteLine("Hello World");
+        }
+    }
+
+
+    // Question 2: Startup/ShutDown messages with timestamps
+
+    void Awake()
+    {
+        Debug.Log("Start Application TimeStamp: " + DateTime.Now);
+    }
+
+    void OnApplicationQuit()
+    {
+        Debug.Log("Quit Application Timestamp: " + DateTime.Now);
+    }
+
+
+
+    // Question 3: User Input Tracking (Spacebar)
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Spacebar Pressed TimeStamp: " + DateTime.Now);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("Spacebar Released TimeStamp: " + DateTime.Now);
+        }
+    }
+
+}
